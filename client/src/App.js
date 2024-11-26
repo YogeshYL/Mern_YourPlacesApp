c83b0964799018964cff12e849501a786016c848
YogeshYL <yogeshsankhla726@gmail.com>
2024-11-26 16:21:18 +0530

    yogesh check

---
+++ client/src/App.js
@@ -0,0 +1,121 @@
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import "./index.css";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
+import { AuthContext } from "./shared/context/auth-context";
+
+// const Users = React.lazy(() => import("./user/pages/Users"));
+// const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+// const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+// const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
+// const Auth = React.lazy(() => import("./user/pages/Auth"));
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
+
+let logoutTimer;
+
+const App = () => {
+  const [token, setToken] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [tokenExpDate, setTokenExpDate] = useState();
+
+  const login = useCallback((uid, token, expireDate) => {
+    setToken(token);
+    setUserId(uid);
+    const expirationDate =
+      expireDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpDate(expirationDate);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: expirationDate.toISOString(),
+      })
+    );
+  }, []);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
+    }
+  }, [login]);
+
+  const logout = useCallback(() => {
+    setToken(null);
+    setUserId(null);
+    setTokenExpDate(null);
+    localStorage.removeItem("userData");
+  }, []);
+
+  useEffect(() => {
+    if (token && tokenExpDate) {
+      const remainingTime = tokenExpDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpDate]);
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn: !!token, login, logout, userId, token: token }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          {token ? (
+            <Switch>
+              <Route path="/" exact>
+                <Users />
+              </Route>
+              <Route path="/:userId/places" exact>
+                <UserPlaces />
+              </Route>
+              <Route path="/places/new" exact>
+                <NewPlace />
+              </Route>
+              <Route path="/places/:placeId" exact>
+                <UpdatePlace />
+              </Route>
+              <Redirect to="/" />
+            </Switch>
+          ) : (
+            <Switch>
+              <Route path="/" exact>
+                <Users />
+              </Route>
+              <Route path="/:userId/places" exact>
+                <UserPlaces />
+              </Route>
+              <Route path="/auth" exact>
+                <Auth />
+              </Route>
+              <Redirect to="/auth" />
+            </Switch>
+          )}
+        </main>
+      </Router>
+    </AuthContext.Provider>
+  );
+};
+
+export default App;
---
+++ client/src/places/pages/NewPlace.js
@@ -0,0 +1,107 @@
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import Input from "../../shared/components/FormElement/Input";
+import Button from "../../shared/components/FormElement/Button";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import "./PlaceForm.css";
+import { useForm } from "../../shared/hooks/form-hook";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import { AuthContext } from "../../shared/context/auth-context";
+import ImageUpload from "../../shared/components/FormElement/ImageUpload";
+
+const NewPlace = () => {
+  const history = useHistory();
+  const auth = useContext(AuthContext);
+  const { isLoading, sendRequest, error, clearError } = useHttpClient();
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      image: {
+        value: null,
+        isValid: false,
+      },
+    },
+    false
+  );
+  const placeSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      const formdata = new FormData();
+      formdata.append("title", formState.inputs.title.value);
+      formdata.append("description", formState.inputs.description.value);
+      formdata.append("address", formState.inputs.address.value);
+      formdata.append("image", formState.inputs.image.value);
+
+      await sendRequest(
+        process.env.REACT_APP_BACKEND_URL + "/places",
+        "POST",
+        formdata,
+        {
+          Authorization: "Bearer" + auth.token,
+        }
+      );
+      history.push("/");
+    } catch (error) {}
+  };
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      <form className="place-form" onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          id="title"
+          type="text"
+          label="Title"
+          element="input"
+          errorText="Please input a valid title"
+          validators={[VALIDATOR_REQUIRE()]}
+          onInput={inputHandler}
+        />
+        <Input
+          id="address"
+          type="text"
+          label="Address"
+          element="input"
+          errorText="Please input a valid address"
+          validators={[VALIDATOR_REQUIRE()]}
+          onInput={inputHandler}
+        />
+        <Input
+          id="description"
+          type="text"
+          label="Description"
+          element="textarea"
+          errorText="Please input a valid description (at least 5 char.)"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          onInput={inputHandler}
+        />
+        <ImageUpload
+          onInput={inputHandler}
+          id="image"
+          errorText="Please provide an image(not more than 5MB)"
+        />
+        <Button disabled={!formState.isValid} type="submit">
+          ADD PLACE
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default NewPlace;
